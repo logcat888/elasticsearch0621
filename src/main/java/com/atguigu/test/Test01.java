07786313c49d213b1ae8498bffa6aef3127b28b0
chenhui <chenhuijlucau2016@126.com>
2020-10-28 18:39:09 +0800

    ES 增加了hello world

---
+++ src/main/java/com/atguigu/test/Test01.java
@@ -0,0 +1,11 @@
+package com.atguigu.test;
+
+/**
+ * @author chenhuiup
+ * @create 2020-10-28 18:38
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
